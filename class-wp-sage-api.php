4f5e4dd3792bf2930b12e0971b356db082728bfc
Brandon Hubbard <bkhubbard@gmail.com>
2020-03-25 16:41:39 -0700

    more moving functions

--- class-wp-sage-api.php
+++ class-wp-sage-api.php
@@ -22,8 +22,6 @@ defined( 'ABSPATH' ) || exit;
 // Require Autoloader.
 // require_once trailingslashit( dirname( __FILE__ ) ) . 'autoloader.php';
 
-
-
 /* Check if class exists. */
 if ( ! class_exists( 'SageAPI' ) ) {
 
@@ -795,46 +793,6 @@ if ( ! class_exists( 'SageAPI' ) ) {
 			return $payloads;
 
 		}
-
-
-		// ############################ ENDPOINTS. #############################
-
-
-		/**
-		 * Get Sales Order History Headers.
-		 *
-		 * @param  array $args [description]
-		 * @return [type]       [description]
-		 */
-		public function get_sales_order_history_headers( $args = array(), $format = '' ) {
-			$response = $this->build_request( 'SO_SalesOrderHistoryHeader', $args )->fetch( array( 'format' => $format ) );
-			return $response;
-		}
-
-		/**
-		 * Sales Order History Header by ID.
-		 *
-		 * @param  [type] $sales_order_id [description]
-		 * @param  array  $args           [description]
-		 * @return [type]                 [description]
-		 */
-		public function get_sales_order_history_header( $sales_order_id, $args = array(), $format = '' ) {
-			$response = $this->build_request( 'SO_SalesOrderHistoryHeader(' . $sales_order_id . ')', $args )->fetch( array( 'format' => $format ) );
-			return $response;
-		}
-
-		/**
-		 * [delete_sales_order_history description]
-		 *
-		 * @param  [type] $sales_order_id [description]
-		 * @param  array  $args           [description]
-		 * @return [type]                 [description]
-		 */
-		public function delete_sales_order_history( $sales_order_id, $args = array(), $format = '' ) {
-			$response = $this->build_request( 'SO_SalesOrderHistoryHeader(' . $sales_order_id . ')', $args, 'DELETE' )->fetch( array( 'format' => $format ) );
-			return $response;
-		}
-
 
 	}
 }
--- src/class-sage-sales-orders.php
+++ src/class-sage-sales-orders.php
@@ -20,6 +20,41 @@ class Sage_Sales_Orders extends SageAPI {
 		parent::__construct();
 	}
 
+	/**
+	 * Get Sales Order History Headers.
+	 *
+	 * @param  array $args [description]
+	 * @return [type]       [description]
+	 */
+	public function get_sales_order_history_headers( $args = array(), $format = '' ) {
+		$response = $this->build_request( 'SO_SalesOrderHistoryHeader', $args )->fetch( array( 'format' => $format ) );
+		return $response;
+	}
+
+	/**
+	 * Sales Order History Header by ID.
+	 *
+	 * @param  [type] $sales_order_id [description]
+	 * @param  array  $args           [description]
+	 * @return [type]                 [description]
+	 */
+	public function get_sales_order_history_header( $sales_order_id, $args = array(), $format = '' ) {
+		$response = $this->build_request( 'SO_SalesOrderHistoryHeader(' . $sales_order_id . ')', $args )->fetch( array( 'format' => $format ) );
+		return $response;
+	}
+
+	/**
+	 * [delete_sales_order_history description]
+	 *
+	 * @param  [type] $sales_order_id [description]
+	 * @param  array  $args           [description]
+	 * @return [type]                 [description]
+	 */
+	public function delete_sales_order_history( $sales_order_id, $args = array(), $format = '' ) {
+		$response = $this->build_request( 'SO_SalesOrderHistoryHeader(' . $sales_order_id . ')', $args, 'DELETE' )->fetch( array( 'format' => $format ) );
+		return $response;
+	}
+
 
 
 }
